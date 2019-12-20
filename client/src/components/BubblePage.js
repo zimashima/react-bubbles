c44bdae5f2066f2b4edcf680b9bf96d56ebe7852
Mashima <52222646+zimashima@users.noreply.github.com>
2019-12-20 11:36:32 -0500

    successfully created login page and successfully GET request using axiosWithAuth and display the data on BubblePage.js

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { axiosWithAuth } from "./utils/axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+
+  useEffect(()=>{
+    axiosWithAuth().get('/colors')
+    .then( res => setColorList(res.data))
+
+  }, [])
+
 
   return (
     <>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,63 @@
-import React from "react";
+import React, {useState} from "react";
+import {axiosWithAuth} from './utils/axiosWithAuth'
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+const Login = (props) => {
+
+  const [credentials, setCredentials] = useState({
+    username: 'Lambda School',
+    password: 'i<3Lambd4'
+ });
+
+  const handleChange = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    })
+  }
+  const login = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post('/login', credentials)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload);
+        props.history.push('/FriendList')
+        })
+      .catch(err=> console.log(err))
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      
+        <form onSubmit={login}>
+        <div className="form-group row">
+          <label for="username" className="col-sm-2 col-form-label">Username</label>
+          <input
+            className="form-control"
+            id="username"
+            type="text"
+            name="username"
+            label="Username"
+            value={credentials.username}
+            onChange={handleChange}
+            />
+          </div>
+          <div className="form-group row">
+            <label for="password" className="col-sm-2 col-form-label">Password</label>
+            <input
+            className="form-control"
+            id="password"
+            type="password"
+            name="password"
+            label="Password"
+            value={credentials.password}
+            onChange={handleChange}
+            />  
+          </div>
+          <div className="form-group row"><button className="btn btn-info">Login</button></div>
+        </form>
+        
+      
     </>
   );
 };
